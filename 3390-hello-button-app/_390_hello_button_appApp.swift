96a036b04098f9fb1c25efabb1f5146f8cf71818
Ibrahim <ialhumaidi@csub.edu>
2025-09-17 12:44:27 -0700

    Initial Commit

---
+++ 3390-hello-button-app/_390_hello_button_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  _390_hello_button_appApp.swift
+//  3390-hello-button-app
+//
+//  Created by Ibrahim Alhumaidi on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct _390_hello_button_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 3390-hello-button-appTests/_390_hello_button_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  _390_hello_button_appTests.swift
+//  3390-hello-button-appTests
+//
+//  Created by Ibrahim Alhumaidi on 9/17/25.
+//
+
+import Testing
+@testable import _390_hello_button_app
+
+struct _390_hello_button_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
